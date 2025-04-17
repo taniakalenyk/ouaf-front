3ec1cb035ca9cf2c9ae672dbabf32482eaedb5d4
Tetiana <taniakalenyk@gmail.com>
2025-04-17 09:18:13 +0200

    polaroid component zoom added

--- src/app/shared/polaroid/polaroid.component.ts
+++ src/app/shared/polaroid/polaroid.component.ts
@@ -1,41 +1,45 @@
-import {Component, Input} from '@angular/core';
-import {NgStyle} from '@angular/common';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-polaroid',
-  imports: [
-    NgStyle
-  ],
+  standalone: true,
+  imports: [NgStyle, NgClass],
   templateUrl: './polaroid.component.html',
-  styleUrls: ['./polaroid.component.scss']
+  styleUrl: './polaroid.component.scss',
 })
-export class PolaroidComponent {
-  @Input() size: 'small' | 'regular' = 'regular';
+export class PolaroidComponent implements OnChanges {
+  @Input() size: 'small' | 'regular' | 'zoom' = 'regular';
   @Input() title: string = '';
   @Input() description: string = '';
   @Input() imageUrl: string = '';
+  @Input() polaroidView: boolean = true;
+  @Input() zoomed: boolean = false;
 
-  default = '/images/gallery/1.jpg'
-
+  default = '/images/gallery/1.jpg';
   width = '';
   height = '';
-  finalImageUrl = '';
+  finalImageUrl = this.imageUrl?.trim() ? this.imageUrl : this.default;
 
-  ngOnInit() {
-    this.setSize(this.size);
-    this.finalImageUrl = this.imageUrl?.trim() ? this.imageUrl : this.default;
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['size'] || changes['zoomed']) {
+      this.setSize();
+    }
+    if (changes['imageUrl']) {
+      this.finalImageUrl = this.imageUrl?.trim() ? this.imageUrl : this.default;
+    }
   }
 
-  setSize(size: 'small' | 'regular') {
+  setSize() {
     const sizes = {
       small: {width: '7.19rem', height: '9rem'},
       regular: {width: '11.25rem', height: '14.13rem'},
+      zoom: {width: '16.875rem', height: '21.195rem'},
     };
 
-    this.width = sizes[size].width;
-    this.height = sizes[size].height;
+    const selectedSize = this.zoomed ? sizes.zoom : sizes[this.size];
+
+    this.width = selectedSize.width;
+    this.height = selectedSize.height;
   }
-
-
 }
-
