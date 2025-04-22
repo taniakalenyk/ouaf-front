778afd9ee39344cf3f3f1871a527968bbd56a0c5
Tetiana <taniakalenyk@gmail.com>
2025-04-22 22:18:30 +0200

    class-card component added and styled

---
+++ src/app/shared/class-card/class-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-card',
+  imports: [],
+  templateUrl: './class-card.component.html',
+  styleUrl: './class-card.component.scss'
+})
+export class ClassCardComponent {
+
+}
