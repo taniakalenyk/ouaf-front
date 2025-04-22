dd8d4cdeedf2e24aa44bb3218d226cd17fa01b8f
Tetiana <taniakalenyk@gmail.com>
2025-04-22 22:23:31 +0200

    first class-card added to dashboard page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ClassCardComponent} from '../../shared/class-card/class-card.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    ClassCardComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
