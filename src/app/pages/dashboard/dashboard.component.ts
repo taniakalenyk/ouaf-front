551a6f3c03f784ac3023cec67d9f8ce33315c1e6
Tetiana <taniakalenyk@gmail.com>
2025-04-25 12:07:02 +0200

    datepicker added and styled

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,16 @@
 import {Component} from '@angular/core';
 import {ClassCardComponent} from '../../shared/class-card/class-card.component';
 import {ToggleComponent} from '../../shared/toggle/toggle.component';
+import {CheckboxComponent} from '../../shared/checkbox/checkbox.component';
+import {Datepicker} from '../../shared/datepicker/datepicker.component';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     ClassCardComponent,
-    ToggleComponent
+    ToggleComponent,
+    CheckboxComponent,
+    Datepicker
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
